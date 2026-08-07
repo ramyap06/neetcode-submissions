0641bf82cb75a246fd6b58540a6c5f334c6281bc
Ramya Prasanna <prasannr@purdue.edu>
2026-08-07 15:20:15 -0400

    Add: string-encode-and-decode - submission-7

---
+++ Data Structures & Algorithms/string-encode-and-decode/submission-7.py
@@ -0,0 +1,29 @@
+class Solution:
+
+    def encode(self, strs: List[str]) -> str:
+        full_string = ""
+        for string in strs:
+            length = len(string)
+            full_string += str(length) + "?" + string
+        return full_string
+
+    def decode(self, s: str) -> List[str]:
+        i = 0
+        n = len(s)
+        word_list = []
+
+        while i < n:
+            j = i
+            word = ""
+
+            while s[j] != "?":
+                j += 1
+            
+            length = int(s[i:j])
+            i = j + 1
+
+            word = s[i:i + length]
+            word_list.append(word)
+            i += length
+
+        return word_list
