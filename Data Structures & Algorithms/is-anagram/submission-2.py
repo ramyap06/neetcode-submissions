1bdb5463f8a8cc401bc0831834d602a43a1d419f
Ramya Prasanna <prasannr@purdue.edu>
2026-08-07 12:45:39 -0400

    Add: is-anagram - submission-2

---
+++ Data Structures & Algorithms/is-anagram/submission-2.py
@@ -0,0 +1,23 @@
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        if len(s) != len(t):
+            return False
+        
+        letter_dict = {}
+
+        for c in s:
+            if c not in letter_dict:
+                letter_dict[c] = 0
+            letter_dict[c] += 1
+        
+        for i in t:
+            if i not in letter_dict or letter_dict[i] == 0:
+                return False
+            
+            letter_dict[i] -= 1
+        
+        for j in letter_dict:
+            if letter_dict[j] > 0:
+                return False
+        return True
+            
